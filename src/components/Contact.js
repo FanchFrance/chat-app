1b76deac1874b3055c846c5d74b795025daf3279
wilder <francois.yhuel@hotmail.fr>
2020-04-13 11:41:37 +0200

    contact-states

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,26 +1,39 @@
 import React from "react";
+import "./Contact.css";
 
-const Contact = props => {
-  const on = props.online;
-  return (
-    <div className="Contact">
-      <img className="avatar" src={props.avatar} alt={props.avatar} />
-      <div className="status">
-        <h2 className="name">{props.name}</h2>
-        <div className="hello">
-          {on ? (
-            <div className="status-online" />
-          ) : (
-            <div className="status-offline" />
-          )}
-          {on ? (
-            <div className="status-text">"Online"</div>
-          ) : (
-            <div className="status-text">"Offline"</div>
-          )}
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      online: props.online
+    };
+  }
+  changeStatus() {
+    const newStatus = !this.state.online;
+    this.setState({ online: newStatus });
+  }
+
+  render() {
+     
+    return (
+      <div className="Contact">
+        <img className="avatar" src={this.props.avatar} alt={this.props.name} />
+        <div>
+          <div className="name">{this.props.name}</div>
+
+          <div className="status">
+            <div
+              onClick={this.changeStatus.bind(this)}
+              className={this.state.online ? "status-online" : "status-offline"}
+            />
+            <p className="status-text">
+              {this.state.online ? "Online" : "Offline"}
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
+
 export default Contact;
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,41 +1,42 @@
-import React from 'react'
-import Contact from './Contact'
+import React from "react";
+import Contact from "./Contact";
 const users = [
-    {
-      name: 'Robert Reyes',
-      avatar: 'https://randomuser.me/api/portraits/men/28.jpg',
-      online: false
-    },
-    {
-      name: 'Nellie Caldwell',
-      avatar: 'https://randomuser.me/api/portraits/women/17.jpg',
-      online: true
-    },
-    {
-      name: "Elle Woods",
-      avatar: "https://media.tenor.com/images/51f2f57be93a6bbc966b4802e0507e7d/tenor.gif",
-      online: true
-    },
-    {
-      name: 'Erica Hunt',
-      avatar: 'https://randomuser.me/api/portraits/women/87.jpg',
-      online: false
-    },
-    {
-      name: 'Juanita Phillips',
-      avatar: 'https://randomuser.me/api/portraits/women/55.jpg',
-      online: true
-    }
-  ];
+  {
+    name: "Robert Reyes",
+    avatar: "https://randomuser.me/api/portraits/men/28.jpg",
+    online: false,
+    text: "Offline"
+  },
+  {
+    name: "Nellie Caldwell",
+    avatar: "https://randomuser.me/api/portraits/women/17.jpg",
+    online: true,
+    text: "Offline"
+  },
+  {
+    name: "Elle Woods",
+    avatar:
+      "https://media.tenor.com/images/51f2f57be93a6bbc966b4802e0507e7d/tenor.gif",
+    online: true,
+    text: "Offline"
+  },
+  {
+    name: "Erica Hunt",
+    avatar: "https://randomuser.me/api/portraits/women/87.jpg",
+    online: false,
+    text: "Offline"
+  },
+  {
+    name: "Juanita Phillips",
+    avatar: "https://randomuser.me/api/portraits/women/55.jpg",
+    online: true,
+    text: "Offline"
+  }
+];
 
-const ContactList = () => (
-    users.map(props =>(
-    <Contact
-    name = {props.name}
-    avatar = {props.avatar}
-    online = {props.online}/>
-    )
-    )
-    )
-    
-    export default ContactList
+const ContactList = () =>
+  users.map(props => (
+    <Contact name={props.name} avatar={props.avatar} online={props.status} />
+  ));
+
+export default ContactList;
